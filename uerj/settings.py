4aea494612dc4108e8473d0d8f72465e39a114d1
Diego Rocha <diego@diegorocha.com.br>
2016-09-03 20:02:03 -0300

    Corrigo configuração de DEBUG para variaveis de ambiente

--- uerj/settings.py
+++ uerj/settings.py
@@ -25,7 +25,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = config('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=False)
 
 # Application definition
 
